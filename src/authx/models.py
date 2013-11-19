21bca67be7896cb3f6cf28e3a7439a75403b7e38
hdknr <gmail@hdknr.com>
2013-11-19 10:57:31 +0900

    for mod_auth_basic authentication

--- src/authx/models.py
+++ src/authx/models.py
@@ -2,7 +2,40 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+ENCS=(
+    ('sha1', 'Secure Hash Algorihm (SHA1)'),
+    ('none','not encrypted (plain text)'),
+    ('crypt', 'UNIX crypt() encryption'),
+    ('scrambled', 'MySQL PASSWORD encryption'),
+    ('md5', 'MD5 hashing'),
+    ('aes', 'Advanced Encryption Standard (AES) encryption'),
+)
+
 class Basic(models.Model):
     user = models.OneToOneField(User)
-    username = models.CharField('UserNmae',max_length="20",)
+    username = models.CharField('Username',max_length="50",)
     password = models.CharField('Password',max_length="50",)
+    enc = models.CharField('Encryption',max_length="10",choices=ENCS,default=ENCS[0][0],)
+    salt =  models.CharField('Salt',max_length="50",)       #: not used
+
+
+from django.contrib.auth.models import User
+import hashlib
+
+old_set_password = User.set_password
+
+def set_password(user, raw_password):
+    if user.id == None:
+        user.save()
+    else:
+        try:
+            # generate password for mod_auth_mysql  Basic Authentication
+            # TODO: get algorighm from basi.enc
+            user.basic.password = hashlib.sha1(raw_password).hexdigest()   
+            user.basic.save()
+        except:
+            pass
+
+    old_set_password(user, raw_password)
+
+User.set_password = set_password
